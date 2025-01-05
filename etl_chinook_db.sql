b82ab3537294e70e96dfd561f217c84919532fdb
Samuelh27 <samuel.holka.lol@gmail.com>
2025-01-05 14:37:09 +0100

    Update etl_chinook_db.sql

--- etl_chinook_db.sql
+++ etl_chinook_db.sql
@@ -17,3 +17,19 @@ SELECT
     `FirstName` || ' ' || `LastName` AS full_name,
     `City`, `State`, `Country`, `Email`
 FROM `customer`;
+
+
+CREATE TABLE dim_track AS
+SELECT 
+    t.`TrackId` AS dim_track_id,
+    t.`Name` AS track_name,
+    a.`Title` AS album_title,
+    ar.`Name` AS artist_name,
+    g.`Name` AS genre,
+    mt.`Name` AS media_type,
+    t.`UnitPrice` AS price
+FROM `track` t
+JOIN `album` a ON t.`AlbumId` = a.`AlbumId`
+JOIN `artist` ar ON a.`ArtistId` = ar.`ArtistId`
+JOIN `genre` g ON t.`GenreId` = g.`GenreId`
+JOIN `mediatype` mt ON t.`MediaTypeId` = mt.`MediaTypeId`;
