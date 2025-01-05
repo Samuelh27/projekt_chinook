690d78267f871fac7b9f6f02c806040979125cb4
Samuelh27 <samuel.holka.lol@gmail.com>
2025-01-05 14:36:49 +0100

    Update etl_chinook_db.sql

--- etl_chinook_db.sql
+++ etl_chinook_db.sql
@@ -1,1 +1,19 @@
+CREATE OR REPLACE TABLE `album` (
+    `AlbumId` INT,
+    `Title` VARCHAR,
+    `ArtistId` INT
+);
 
+INSERT INTO `album` (`AlbumId`, `Title`, `ArtistId`)
+VALUES 
+    (1, 'For Those About To Rock We Salute You', 1),
+    (2, 'Balls to the Wall', 2),
+    (3, 'Restless and Wild', 2),
+    ...
+  
+CREATE TABLE dim_customer AS
+SELECT 
+    `CustomerId` AS dim_customer_id,
+    `FirstName` || ' ' || `LastName` AS full_name,
+    `City`, `State`, `Country`, `Email`
+FROM `customer`;
